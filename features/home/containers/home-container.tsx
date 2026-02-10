adf4c95f891acd24429b3cd8e5164951b749e73e
Januard Lumbangaol <januargaol48@gmail.com>
2026-02-10 21:43:26 +0700

    form subscribe

--- features/home/containers/home-container.tsx
+++ features/home/containers/home-container.tsx
@@ -1,7 +1,7 @@
 import FeaturedTour from "../components/featured-tour"
 import HeroComponent from "../components/hero"
 import StepOrder from "../components/step-order"
-import SubscribeForm from "../components/subscribe-from"
+import SubscribeForm from "../components/subscribe-form"
 
 const HomeContainer = () => {
     return (
