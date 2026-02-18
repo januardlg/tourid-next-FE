643960a9a80dba347363f4b69878c32b0ceaab2e
januardlg <januargaol48@gmail.com>
2026-02-18 14:43:56 +0700

    provide pagination

--- features/our-tour/components/pagination.tsx
+++ features/our-tour/components/pagination.tsx
@@ -1,22 +1,54 @@
 'use client'
 import React, { useState } from "react"
 import ArrowNextIcon from "@/components/icons/arrow-next-icon"
+import { cn } from "@/lib/utils"
 
 const Pagination = () => {
 
-    // const totalPagesFromResponse = 5
+    const totalPagesFromResponse = 5
     // const LIMIT: number = 5
+    const [activePage, setActivePage] = useState(1)
 
-    // const [page, setPage] = useState<number>(1)
+    const arrTotalPages = Array.from({ length: totalPagesFromResponse }, (_, index) => index + 1)
 
 
+
+    const handleNextPage = () => {
+        setActivePage((prevState: number) => {
+            if (prevState < totalPagesFromResponse) {
+                return prevState + 1
+            } else {
+                return prevState
+            }
+        })
+    }
+
+    const handlePrevPage = () => {
+        setActivePage((prevSate) => {
+            if (prevSate > 1) {
+                return prevSate - 1
+            } else return 1
+        })
+    }
 
     return (
         <section className="mt-15 w-full flex justify-center items-center">
-            <div className="flex-align-items-center gap-7 text-lg font-medium">
-                <div>01</div>
-                <div>02</div>
-                <ArrowNextIcon />
+            <div className="flex-align-items-center gap-7">
+                <button className={
+                    cn('opacity-100 transition duration-300 ease-in-out rotate-180 cursor-pointer', activePage === 1 && 'opacity-0')
+                } onClick={handlePrevPage}>
+                    <ArrowNextIcon />
+                </button>
+                {arrTotalPages.slice(activePage - 1, activePage + 1).map((val: number) => (
+                    <div key={val} className={cn("text-lg font-medium text-tid-grey-200", activePage === val && 'font-bold text-black')}>{val}</div>
+                ))}
+                <button className={
+                    cn('opacity-100 transition duration-300 ease-in-out cursor-pointer', activePage === totalPagesFromResponse && 'opacity-0')
+                } onClick={handleNextPage}>
+                    <ArrowNextIcon />
+                </button>
+
+
             </div>
         </section>
     )
