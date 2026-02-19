6abf5562008ce2a7d0dee4138ab05d71f8bf9b4e
januardlg <januargaol48@gmail.com>
2026-02-19 14:51:29 +0700

    provide history

---
+++ app/(customer)/trip-history/page.tsx
@@ -0,0 +1,10 @@
+import TripHistoryContainer from "@/features/trip-history/containers/trip-history-container"
+
+
+const Page = async () => {
+    return (
+        <TripHistoryContainer />
+    )
+}
+
+export default Page
